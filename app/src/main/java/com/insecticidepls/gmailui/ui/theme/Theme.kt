5fef312dccb5c23af46e78c796c796f2968a367e
Renato Santos Nascimento <InsecticidePls@protonmail.com>
2023-01-11 01:56:52 -0300

    [06] - Removed theme related code. i'll try again later

--- app/src/main/java/com/insecticidepls/gmailui/ui/theme/Theme.kt
+++ app/src/main/java/com/insecticidepls/gmailui/ui/theme/Theme.kt
@@ -1,11 +1,9 @@
 package com.insecticidepls.gmailui.ui.theme
 
-import android.app.Activity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalView
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
@@ -18,12 +16,6 @@ fun IPGmailUITheme(
     content: @Composable () -> Unit
 ) {
     LightColorPalette
-
-    val view = LocalView.current
-    val activity = view.context as Activity
-    val window = (activity).window
-
-    window.context
 
     MaterialTheme(
         colors = colors,
